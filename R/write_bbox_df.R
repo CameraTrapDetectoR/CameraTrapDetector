3617c1b7ea2f816329ef341222f32696dbafd6c8
burnsal <amira.burns@usda.gov>
2025-01-21 17:17:26 -0700

    simplify function

--- R/write_bbox_df.R
+++ R/write_bbox_df.R
@@ -4,26 +4,17 @@
 #' 
 #' @param full_df prediction output from apply_score_threshold
 #' @param bboxes existing predicted boxes df
-#' @param score_threshold confidence score to filter bounding boxes
 #'
 #' @import dplyr
 #'
 #' @export
-write_bbox_df <- function(full_df, bboxes, score_threshold) {
+write_bbox_df <- function(full_df, bboxes) {
   
   # convert list into dataframe
-  # predictions_df <- do.call(dplyr::bind_rows, predictions_list)
+  bbox_df <- do.call(dplyr::bind_rows, predictions_list)
   
-  bbox_df <- data.frame("filename" = full_df$filename,
-                        "prediction" = full_df$prediction,
-                        "confidence_score" = full_df$confidence_score,
-                        "XMin" = round(full_df$XMin, 3),
-                        "XMax" = round(full_df$XMax, 3),
-                        "YMin" = round(full_df$YMin, 3),
-                        "YMax" = round(full_df$YMax, 3))
- 
-  # filter out predicted bboxes below score_threshold
-  bbox_df <- bbox_df[bbox_df$confidence_score >= score_threshold, ]
+  # round bbox coordinates to the third decimal place
+  bbox_df <- dplyr::mutate(bbox_df, dplyr::across(x_center:YMax), ~round(., 3))
   
   # combine new bboxes with any existing results
   bbox_df <- unique(rbind(bbox_df, bboxes))
